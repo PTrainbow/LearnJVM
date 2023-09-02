e2c8c6096c1ccfaa1879d0f7bc9b599be32d4744
PTrainbow <327365600@qq.com>
2023-09-02 12:48:57 +0800

    learn jvm ch01

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+extern crate structopt;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name="LearnJVM", usage="Usage: LearnJVM [-options] class [args...]")]
+pub struct Options {
+    #[structopt(long="version", help="print version message")]
+    version_flag: bool,
+    #[structopt( long="classpath", help= "classpath",  takes_value=true)]
+    classpath: Option<String>,
+    #[structopt( long="cp", help= "classpath",  takes_value=true)]
+    cp: Option<String>,
+    #[structopt(takes_value=true)]
+    class: Option<String>,
+    #[structopt(takes_value=true, multiple=true)]
+    args: Vec<String>
+}
+
+fn main() {
+    let options = Options::from_args();
+    if options.version_flag {
+        print!("version: 0.0.1\n");
+        return;
+    } else if (options.classpath.is_some() || options.cp.is_some()) {
+        println!("parse input {:?}\n", options);
+    } else {
+        print!("Usage: LearnJVM [-options] class [args...]\n")
+    }
+}
